1f4c91ff0d028deb1ff2f3b92dd0e972613236ee
PC Owner <niqevans@gmail.com>
2021-04-21 12:32:31 -0400

    added an if else tree for the inclusion of gender for the scholarship

--- pythonassignment6.py
+++ pythonassignment6.py
@@ -26,16 +26,11 @@ mailing_address3 = input("Enter Country/Region:")
 
 # Student is from Africa use A as an output if the student is not from Africa then use O
 native = str(input("Are you from Africa(Y/N):"))
-if native == "Y":
-    native = "A"
-    native1 = "Yes"
-else:
-    native = "O"
-    native1 = "No"
 
 # Students Email
 email = input("Enter E-mail:")
 
+student_gender = str(input("Enter the student's gender[M] for Male or [F] for Female: "))
 # Students average score of Quiz1 Quiz2 Quiz3 Test1 and Test2
 
 Quiz1 = float(input("enter Quiz1 score **out of 100** : "))
@@ -43,7 +38,7 @@ Quiz2 = float(input("enter Quiz2 score **out of 100** : "))
 Quiz3 = float(input("enter Quiz3 score **out of 100** : "))
 Test1 = float(input("enter Test1 score **out of 100** : "))
 Test2 = float(input("enter Test2 score **out of 100** : "))
-Average_score = ((Quiz1 + Quiz2 + Quiz3 + Test1 + Test2) / 500) * 100
+average_score = float(((Quiz1 + Quiz2 + Quiz3 + Test1 + Test2) / 500) * 100)
 
 # Display student score on zoom call [0-9] points
 zoom_score = int(input("Enter zoom attendance score **numbers from 0-9**:"))
@@ -53,7 +48,23 @@ homework_score = int(input("Enter homework assignment score **numbers from 0-10*
 
 # If student is not based in Africa then do not offer them the scholarship only offer it if they are from Africa
 
-Scholarship_candidate = native1
+if native == "Y":
+    native1 = "A"
+    if student_gender == "M":
+        if average_score >= 80:
+            scholarship_candidate = "Yes"
+        else:
+            scholarship_candidate = "No"
+    elif student_gender == "F":
+        if average_score >= 76:
+            scholarship_candidate = "Yes"
+        else:
+            scholarship_candidate = "No"
+    else:
+        student_gender = input("Use M or F for student's gender: ")
+else:
+    native1 = "O"
+    scholarship_candidate = "No"
 
 # format an appropriate output
 print("Student's Full name (First,Last): " + full_name)
@@ -62,8 +73,9 @@ print("Student's ID number: " + student_ID_number)
 print("Student's Mailing address: " + mailing_address1)
 print("Student's City residence: " + mailing_address2)
 print("Student's Country/Region of residence: " + mailing_address3)
-print("African region: " + native)
-print("Student's Quiz and Test average: " + str(Average_score))
+print("African region: " + native1)
+print("Student's Quiz and Test average: " + str(average_score))
+print("Student's Gender: " + student_gender)
 print("Student's Attendance score: " + str(zoom_score))
 print("Student's Homework score: " + str(homework_score))
-print("Scholarship candidate: " + Scholarship_candidate)
+print("Scholarship candidate: " + scholarship_candidate)
